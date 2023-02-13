7b8b2dc68f00bcdb11f924b88e4b2a92fe4b0ad2
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-02-13 09:27:25 +0200

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1
+{
+    class Driver
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public float TotalTime { get; set; }
+
+        public float Speed { get; set; }
+
+        
+
+        public Driver(string name, int age, float totaltime, float speed)
+        {
+            Name = name;
+            Age = age;
+            TotalTime = totaltime;
+            Speed = speed;
+            
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace F1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<Driver>();
+            float b = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string name = input[0] +" "+ input[1];
+                int age = int.Parse(input[2]);
+                float totalTime = float.Parse(input[3]);
+                float speed = float.Parse(input[4]);
+
+
+
+                var driver = new Driver(name, age, totalTime, speed);
+                list.Add(driver);
+
+
+            }
+
+            foreach(var item in list)
+            {
+                if(item.Speed > b)
+                {
+                    b = item.Speed;
+                }
+            }
+
+            var fr = list.Find(x => x.Speed == b);
+
+            Console.WriteLine("DriverName: " + fr.Name);
+            Console.WriteLine(" DriverAge: " + fr.Age);
+            Console.WriteLine("  Time: " + fr.TotalTime);
+            Console.WriteLine("  Speed: " + fr.Speed);
+
+
+
+        }
+
+        
+    }
+}
